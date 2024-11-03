2dbee1a6e38226947a9940b12087273fc6e6a848
VarshithaK22 <varshitha.k344@gmail.com>
2024-11-03 12:13:38 +0530

    login form testing with bootstrap5 styling

--- diary_project/diary/views.py
+++ diary_project/diary/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm#, ProfileForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        # profile_form = ProfileForm(request.POST)
+        
+        if user_form.is_valid():
+        # and profile_form.is_valid():
+            user = user_form.save()
+            # user.profile.date_of_birth = profile_form.cleaned_data.get('date_of_birth')
+            user.profile.save()
+            
+            login(request, user)
+            messages.success(request, 'Registration successful!')
+            return redirect('home')  # Change to your home view
+    else:
+        user_form = UserRegistrationForm()
+        # profile_form = ProfileForm()
+    
+    return render(request, 'register.html', {
+        'user_form': user_form,
+        # 'profile_form': profile_form
+    })
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
--- diary_project/diary_project/settings.py
+++ diary_project/diary_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'diary',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +72,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'diary_project.wsgi.application'
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
