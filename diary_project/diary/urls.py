c94cbbf660dbee58b3f0f80e02e79d4e61d01c89
VarshithaK22 <varshitha.k344@gmail.com>
2024-11-03 15:04:33 +0530

    Fixed logout button issue

--- diary_project/diary/urls.py
+++ diary_project/diary/urls.py
@@ -5,7 +5,11 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(
+                                                  next_page='/',
+                                                  http_method_names=['get', 'post'],
+                                                #   template_name='logout.html'
+                                                  ), name='logout'),
     path('profile/', views.profile, name='profile'),
     path('', views.EntryListView.as_view(), name='home'),
 ]
