a756578c17932ca5017d583853b8935aad5ae0cd
VarshithaK22 <varshitha.k344@gmail.com>
2024-11-04 11:36:58 +0530

    added mood

---
+++ diary_project/diary/templatetags/emoji_filters.py
@@ -0,0 +1,8 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def get_emoji(value):
+    # Split the string and return the last part (emoji)
+    return value.split()[-1]
